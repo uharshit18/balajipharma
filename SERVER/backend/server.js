f4bb243b7e201c4d90e7f48f456e3dbf7332a5f4
uharshit18 <uharshit18@gmail.com>
2025-12-07 12:24:04 +0530

    BACKEND FIX AFTER LAST COMMIT

--- SERVER/backend/server.js
+++ SERVER/backend/server.js
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// --- ROOT ROUTE ---
+app.get('/', (req, res) => {
+  res.send('Balaji Pharma Backend is Running');
+});
+
 // --- CONFIGURATION ---
 const SPREADSHEET_ID = '1Lo9PeVltjDkXLkYq7jkgEsrn2-mIA-uPcEm9PC5xRbM';
 
